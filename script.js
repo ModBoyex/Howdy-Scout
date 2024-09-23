5f1de191eb5644c2bc72a98d5cef1a215d94c9a1
ModBoyex <79735432+ModBoyex@users.noreply.github.com>
2024-09-23 10:21:23 -0500

    Beginnings of a graph

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let team_index = 0;
 let team_stat_index = 0;
 
 let event_key;
+let current_stat;
 
 var event_dropdown = document.getElementById("event_dropdown");
 var option_template = document.getElementById("option").cloneNode();
@@ -14,12 +15,17 @@ var team_stat_chart = document.getElementById("team_stat_chart");
 var team_stat_chart_labels = document.getElementById("team_stat_chart_labels");
 var team_stat_chart_template = document.getElementById("team_stat_chart_option").cloneNode(true);
 
+var scout_stat_picker = document.getElementById("stat_dropdown");
+var scout_stat_template = document.getElementById("stat_option").cloneNode(true);
+
 const team_number = 6377;
 const year = new Date().getFullYear();
 const APIKey = "UVsAfK9zInMmgPxhfmdEvqPThM51zgyZL7sP6mLFRTPAZtvzbwFyL6yifjbnvcbU";
-
 
 document.getElementById("team_stat_chart_option").remove();
 
 document.addEventListener("DOMContentLoaded", fetchTeamInfo);
 event_dropdown.onchange = fetchEventTeamsInfo;
+
+setupStatPicker();
+scout_stat_picker.onchange = updateStatGraph;
